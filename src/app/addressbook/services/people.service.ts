3e91cc64214558f7ade917be03da5906627bfb89
Dale <S00123318@mail.itsligo.ie>
2022-02-23 15:07:49 +0000

    step1+2 Initialisation

--- src/app/addressbook/services/people.service.ts
+++ src/app/addressbook/services/people.service.ts
@@ -10,8 +10,8 @@ export class PeopleService {
   http: HttpClient;
   // create your own mock api addressbook containing
   // id, lastname, firstname, phone
-  apiURL="https://620e616c585fbc3359e05f1a.mockapi.io";
-
+ // apiURL="https://620e616c585fbc3359e05f1a.mockapi.io";
+  apiURL="https://621644dc7428a1d2a3617897.mockapi.io";
 
 
 
@@ -20,28 +20,28 @@ export class PeopleService {
   }
 
   getAllPeople(): Observable<People[]> {
-    return this.http.get<People[]>(this.apiURL+'/api/vi/people?sortBy=lastname')
+    return this.http.get<People[]>(this.apiURL+'/api/v1/people?sortBy=lastname')
       .pipe(
         tap(people=>console.log(JSON.stringify(people))
         ));
   }
 
   createPerson(people: People): Observable<People> {
-    return this.http.post<People>(this.apiURL+'/api/vi/people', people).pipe(
+    return this.http.post<People>(this.apiURL+'/api/v1/people', people).pipe(
       tap(value => {
       })
     );
   }
 
   deletePerson(personId: string): Observable<any> {
-    return this.http.delete(this.apiURL+'/api/vi/people/' + personId).pipe(
+    return this.http.delete(this.apiURL+'/api/v1/people/' + personId).pipe(
       tap(result => {
       })
     );
   }
 
   updatePerson(personId: string, person: People): Observable<any> {
-    return this.http.put(this.apiURL+'/api/vi/people/' + personId, person).pipe(
+    return this.http.put(this.apiURL+'/api/v1/people/' + personId, person).pipe(
       tap(result => {
       })
     );
